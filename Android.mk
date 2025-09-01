bfa91ad4bd8a1a15cc9eee1091e5bc14e784fe56
Vivekachooz <vivekachooz@gmail.com>
2025-09-01 11:24:04 +0530

    denver: Import initial device specific blobs
    * From denver_global S2RE32.29-16-1-5-18 build
    
    Change-Id: Ic0c563aedd281c4660f4cf75db4169b8eee9661c

---
+++ Android.mk
@@ -0,0 +1,27 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),denver)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,0e9c1c0870265bed492df1cccca1c001392d9aa8)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,3982f4992c609517675064b6934b9b8892d86055)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,1f76bafd10d298e7a8f273391a5e20e811833089)
+$(call add-radio-file-sha1-checked,radio/dsp.img,527fa9c164649142096d4999d94809e814118b59)
+$(call add-radio-file-sha1-checked,radio/fsg.img,8a9f326ae863474a4eb8546ba06fdce94108aa43)
+$(call add-radio-file-sha1-checked,radio/hyp.img,3d92d908b5c69c034ea52b27b4fb45418068d277)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,05b4088fa038ed93276d7f6eb12874ff20c0ab3b)
+$(call add-radio-file-sha1-checked,radio/logo.img,70f2dac878ddd3c63924da84a74c86c1a5466714)
+$(call add-radio-file-sha1-checked,radio/modem.img,fb491562be427633d0b5ec17008e6f2b09ff6c45)
+$(call add-radio-file-sha1-checked,radio/prov.img,49f1a32ef2159b8c9bde6327ec20a60c37497bc2)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,7750db6445295303efe38e9832977ee10153583b)
+$(call add-radio-file-sha1-checked,radio/rpm.img,bddf42a450cb7697719b72e453f3457236155244)
+$(call add-radio-file-sha1-checked,radio/storsec.img,c1ac4942f3f24a4797492c3842067d2c5a3077e8)
+$(call add-radio-file-sha1-checked,radio/tz.img,858b874f558458658915215eee93d42647664e56)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,a6191ef191625d577ef8add4ce51be1bf0184721)
+$(call add-radio-file-sha1-checked,radio/xbl.img,cb48d88688b57e34055bf431efe0b0e82a3a32bd)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,7e637bad60f58310ebbdb81f7d879e8c758416a3)
+
+endif
